4ddcb4ecbf1abce37c8946b66df31eb5962f286f
Ady Ngom <adyngom.com@gmail.com>
2012-11-02 18:40:53 -0700

    adding custom css for flexslider

--- jetro/header.php
+++ jetro/header.php
@@ -12,6 +12,7 @@
     <link href="css/custom.css" rel="stylesheet" />
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
     <link href="css/flexslider/flexslider.css" rel="stylesheet" />
+    <link href="css/custom/main-slider.css" rel="stylesheet" />
     <link href="css/custom-responsive.css" rel="stylesheet" />
     <link href="css/fonts/bebas/stylesheet.css" rel="stylesheet">
   </head>
--- jetro/sliders/home-flexslider.php
+++ jetro/sliders/home-flexslider.php
@@ -1,7 +1,10 @@
 <div class="container header full-header" >
 	<!-- Place somewhere in the <body> of your page -->
-<div id="slider" class="flexslider">
+<div id="slider" class="flexslider jetro-slider">
   <ul class="slides">
+  	<li>
+      <img src="img/slides/s_1.jpg" />
+    </li>
     <li>
       <img src="img/slides/p_1.jpg" />
     </li>
@@ -13,12 +16,18 @@
     </li>
     <li>
       <img src="img/slides/p_4.jpg" />
+    </li>
+    <li>
+      <img src="img/slides/s_2.jpg" />
     </li>
     <!-- items mirrored twice, total of 12 -->
   </ul>
 </div>
-<div id="carousel" class="flexslider">
+<div id="carousel" class="flexslider jetro-slider">
   <ul class="slides">
+  	<li>
+      <img src="img/slides/s_1.jpg" />
+    </li>
     <li>
       <img src="img/slides/p_1.jpg" />
     </li>
@@ -30,6 +39,9 @@
     </li>
     <li>
       <img src="img/slides/p_4.jpg" />
+    </li>
+    <li>
+      <img src="img/slides/s_2.jpg" />
     </li>
     <!-- items mirrored twice, total of 12 -->
   </ul>
